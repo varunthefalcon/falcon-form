b5207982cdbe5f17bba0a60e6a0bb7db6668e6f0
varunsapp <varuns@apptomate.co>
2020-01-15 23:28:10 +0530

    init create-react-hook@3.0.0

---
+++ README.md
@@ -0,0 +1,34 @@
+# falcon-form
+
+> easy form and error handle
+
+[![NPM](https://img.shields.io/npm/v/falcon-form.svg)](https://www.npmjs.com/package/falcon-form) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install --save falcon-form
+```
+
+## Usage
+
+```jsx
+import React, { Component } from 'react'
+
+import { useMyHook } from 'falcon-form'
+
+const Example = () => {
+  const example = useMyHook()
+  return (
+    <div>{example}</div>
+  )
+}
+```
+
+## License
+
+MIT © [varunthefalcon](https://github.com/varunthefalcon)
+
+---
+
+This hook is created using [create-react-hook](https://github.com/hermanya/create-react-hook).
---
+++ src/index.js
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+
+const useValidateForm = (initValues, successCallback, fieldValidators) => {
+  const [values, setValues] = useState({ ...initValues })
+
+  const [dirtyInit, setDirtyInit] = useState({ ...initValues })
+
+  if (
+    dirtyInit !== true &&
+    JSON.stringify(dirtyInit) !== JSON.stringify(initValues)
+  ) {
+    setValues({ ...initValues })
+    setDirtyInit(true)
+  }
+
+  const [errors, setErrors] = useState({})
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const validate = fieldValues => {
+    let newErrors = {}
+    let oldErrors = { ...errors }
+    Object.keys(fieldValues).forEach(field => {
+      const value = fieldValues[field]
+      let valitationExps = fieldValidators[field]
+      if (typeof valitationExp === 'function') {
+        valitationExps = [valitationExps]
+      }
+      Array.isArray(valitationExps) &&
+        valitationExps.forEach(valitationExp => {
+          if (
+            typeof valitationExp === 'function' &&
+            !newErrors[field] &&
+            valitationExp(value, values)
+          ) {
+            if (oldErrors[field]) {
+              return
+            }
+            newErrors[field] = valitationExp(value)
+          } else {
+            delete oldErrors[field]
+          }
+        })
+    })
+    return { ...oldErrors, ...newErrors }
+  }
+
+  const formSubmit = event => {
+    event.preventDefault()
+    let anyErrors = validate(values)
+    if (Object.keys(anyErrors).length === 0) {
+      setIsSubmitting(true)
+      successCallback()
+      setIsSubmitting(false)
+    } else {
+      setErrors(anyErrors)
+    }
+  }
+
+  const fieldChange = event => {
+    event.preventDefault()
+    let { name, value } = event.target
+    setValues(values => ({
+      ...values,
+      [name]: value
+    }))
+    setErrors(validate({ [name]: value }))
+  }
+
+  return {
+    fieldChange,
+    formSubmit,
+    values,
+    errors
+  }
+}
+
+export default useValidateForm
